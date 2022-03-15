906202b2c15aa47149dcab5751487f3b6c797150
Aghla Taoufik <ag.taoufik@gmail.com>
2022-03-15 12:07:59 +0300

    nationalite Structure

---
+++ database/factories/NationaliteFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Nationalite;
+use Faker\Generator as Faker;
+
+$factory->define(Nationalite::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
