9f047c1d9ef4f57ba28f1835785fb1d07a2d0871
Aghla Taoufik <ag.taoufik@gmail.com>
2022-03-21 11:57:35 +0300

    Fix Translate

--- resources/lang/ar/auth.php
+++ resources/lang/ar/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'هذه البيانات غير متطابقة مع قاعدة البيانات.',
+    'throttle' => 'عدد محاولات الدخول كبير جدا. يرجى المحاولة مرة أخرى بعد :seconds ثانية.',
 
 ];
