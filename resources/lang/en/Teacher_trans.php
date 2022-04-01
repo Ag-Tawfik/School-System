0e961a7009dfff83c8ef3a030c65d6664d67c705
Aghla Taoufik <ag.taoufik@gmail.com>
2022-04-01 09:14:07 +0300

    Add Teacher Trans

---
+++ resources/lang/en/Teacher_trans.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Name_Teacher' => 'Name Teacher',
+    'Add_Teacher' => 'Add Teacher',
+    'Edit_Teacher' => 'Edit Teacher',
+    'Delete_Teacher' => 'Delete Teacher',
+    'Email' => 'Email',
+    'Password' => 'Password',
+    'Name_ar' => 'Teacher Name in Arabic',
+    'Name_en' => 'Teacher Name in English',
+    'specialization' => 'Specialization',
+    'Gender' => 'Gender',
+    'Joining_Date' => 'Joining Date',
+    'Address' => 'Address',
+];
