6f69965a17052a6405447e503653409dab491d54
Aghla Taoufik <ag.taoufik@gmail.com>
2022-04-01 14:54:18 +0300

    Add Validition

--- app/Repository/TeacherRepository.php
+++ app/Repository/TeacherRepository.php
@@ -27,21 +27,21 @@ class TeacherRepository implements TeacherRepositoryInterface
 
     public function StoreTeachers($request)
     {
-        try {
-            $Teachers = new Teacher();
-            $Teachers->Email = $request->Email;
-            $Teachers->Password =  Hash::make($request->Password);
-            $Teachers->Name = ['en' => $request->Name_en, 'ar' => $request->Name_ar];
-            $Teachers->Specialization_id = $request->Specialization_id;
-            $Teachers->Gender_id = $request->Gender_id;
-            $Teachers->Joining_Date = $request->Joining_Date;
-            $Teachers->Address = $request->Address;
-            $Teachers->save();
-            toastr()->success(trans('messages.success'));
-            return redirect()->route('Teachers.create');
-        } catch (Exception $e) {
-            return redirect()->back()->with(['error' => $e->getMessage()]);
-        }
+            try {
+                $Teachers = new Teacher();
+                $Teachers->Email = $request->Email;
+                $Teachers->Password =  Hash::make($request->Password);
+                $Teachers->Name = ['en' => $request->Name_en, 'ar' => $request->Name_ar];
+                $Teachers->Specialization_id = $request->Specialization_id;
+                $Teachers->Gender_id = $request->Gender_id;
+                $Teachers->Joining_Date = $request->Joining_Date;
+                $Teachers->Address = $request->Address;
+                $Teachers->save();
+                toastr()->success(trans('messages.success'));
+                return redirect()->route('Teachers.create');
+            } catch (Exception $e) {
+                return redirect()->back()->with(['error' => $e->getMessage()]);
+            }
     }
 
     public function editTeachers($id)
