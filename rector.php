186dca45b9a824bf13d4b3dfd8fd75c58616ec03
AgTaoufik <ag.taoufik@gmail.com>
2023-02-01 15:28:09 +0300

    Add rector/ecs

--- rector.php
+++ rector.php
@@ -10,13 +10,6 @@ use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/app',
-        __DIR__ . '/bootstrap',
-        __DIR__ . '/config',
-        __DIR__ . '/database',
-        __DIR__ . '/public',
-        __DIR__ . '/resources',
-        __DIR__ . '/routes',
-        __DIR__ . '/storage',
         __DIR__ . '/tests',
     ]);
 
